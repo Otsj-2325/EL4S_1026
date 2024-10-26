e80f81d7331ba6990fefc70ca9d11a8af78379fd
H.Ototsuji <nkjthat25@gmail.com>
2024-10-26 12:12:36 +0900

    food prefab 作成

---
+++ Assets/Otsj/FoodData.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Food : MonoBehaviour
+{
+    #region 設定パラメータ
+    [Header("食材のパラメータ")]
+
+    [SerializeField][Tooltip("食材名")]
+    string foodName = "";
+
+    [SerializeField]
+    [Tooltip("テクスチャ")]
+    Sprite texture = null;
+
+    [SerializeField][Tooltip("カロリー値")]
+    int calorie = 0;
+
+    #endregion
+
+
+    #region 処理変数
+    private SpriteRenderer sprender;
+
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent<SpriteRenderer>(out sprender);
+        sprender.sprite = texture;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
